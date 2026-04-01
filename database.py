0822dd5625175cd491156dc8ee2005417dbca690
Orzan Ingrid <ingrid.d.orzan@gmail.com>
2026-04-01 11:11:06 +0300

    am comentat database.py

--- database.py
+++ database.py
@@ -1,16 +1,16 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import create_engine, Column, Integer, String
+# from sqlalchemy.orm import sessionmaker, declarative_base
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = 'mssql+pyodbc://DESKTOP-O6MHKP8/proiect?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
+# DATABASE_URL = 'mssql+pyodbc://DESKTOP-O6MHKP8/proiect?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
 
-engine = create_engine(DATABASE_URL)
-Base = declarative_base()
-# Test connection
-try:
-    with engine.connect() as connection:
-        print("Database connected successfully!")
-except Exception as e:
-    print(f"Error connecting to database: {e}")
+# engine = create_engine(DATABASE_URL)
+# Base = declarative_base()
+# # Test connection
+# try:
+#     with engine.connect() as connection:
+#         print("Database connected successfully!")
+# except Exception as e:
+#     print(f"Error connecting to database: {e}")
 
