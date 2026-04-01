c76072b1b1a0a977dfe37f4ab10fc3e760f7bf03
Orzan Ingrid <ingrid.d.orzan@gmail.com>
2026-04-01 11:08:22 +0300

    am comentat database.py

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = 'mssql+pyodbc://DESKTOP-O6MHKP8/proiect?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
 
 engine = create_engine(DATABASE_URL)
-
+Base = declarative_base()
 # Test connection
 try:
     with engine.connect() as connection:
